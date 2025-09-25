2ee0c44e8896daf391dde0a581b4245630e6624e
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-09-25 01:18:04 -0700

    little change
    renaming and  remove some lines

--- twrp_KINGKONG_9.mk
+++ twrp_KINGKONG_9.mk
@@ -16,14 +16,9 @@ $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, device/cubot/KINGKONG_9/device.mk)
 
 PRODUCT_DEVICE := KINGKONG_9
-PRODUCT_NAME := omni_KINGKONG_9
+PRODUCT_NAME := twrp_KINGKONG_9
 PRODUCT_BRAND := CUBOT
 PRODUCT_MODEL := KINGKONG 9
 PRODUCT_MANUFACTURER := cubot
 
 PRODUCT_GMS_CLIENTID_BASE := android-cubot
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vext_k6789v1_64-user 12 SP1A.210812.016 p1rck61v164bspP9 release-keys"
-
-BUILD_FINGERPRINT := CUBOT/KINGKONG_9_EEA/KINGKONG_9:14/UP1A.231005.007/1749708454:user/release-keys
