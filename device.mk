7a5e17642343ad365ea1198fe18b7c433feeccdc
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-09-25 01:20:06 -0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -7,10 +7,6 @@
 
 LOCAL_PATH := device/cubot/KINGKONG_9
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
